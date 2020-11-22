3dc43d1bcc9e80c1b6d410d4a414923897e06a6d
hyunjusong <hun3780@gmail.com>
2020-11-22 20:22:06 +0900

    feature/#3/로그인 화면 구현 1차버전_모바일종류에 따른 비율깨지는 문제 해결해야함

--- App.js
+++ App.js
@@ -1,21 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View } from 'react-native';
+import Login from './src/components/Login';
+import Home from './src/components/Home';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [isLogin, setIsLogin] = useState(false);
+  return <View>{isLogin ? <Home /> : <Login />}</View>;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
