48525a8672a132d1ead14fdf8565e50386159ac7
문상현 <hdaleee@munsanghyeon-ui-MacBookAir.local>
2020-11-25 02:53:04 +0900

    #4/:art:/mainPage/CSS완료
    1. Alarm.js 기껏 작성했는데 flatList에서 first-child를 선택하는 방법을 못찾아서 도루묵으로 map을 활용했다. 굳이 map에서도 Alarm.js를 쓰지 않은 데에도 역시 first-child를 선택하는데 어려움이 있기 때문이었다. 결국 삼항연산으로 해결은 했으나 폰트색상이 안바뀐다.. 속성 상속 우선순위가 CSS와 달라 많이 헷갈린다.
    2. AlarmList.js 위와 같은 이유로 사용되지 않았다. 권고사항으로는 FlatList를 활용해서 작업하는게 맞는듯하다.
    3. HomeScreen/index.js 정말 대대적인 변화가 있었다. 기존의 react와는 다르게 map을 사용하는 것이 일반적이지 않았고, FlatList를 보편적으로 사용한다.

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import { View, Text, Dimensions } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Dimensions, FlatList, StyleSheet, ScrollView } from 'react-native';
 import CountdownTimer from '../../components/CountdownTimer';
-import AlarmList from '../../components/AlarmList';
+// import AlarmList from '../../components/AlarmList';
+import Alarm from '../../components/Alarm';
 
 // Get Today Checked _주간 복용 현황
 const fakeGetTodayChecked = { 1: [true, true, true, true, false, true, false] };
 const totalCount = fakeGetTodayChecked[1].length;
-let checkCounting = function () {
+const checkCounting = function () {
   let result = 0;
   for (let m = 0; fakeGetTodayChecked[1][m] < totalCount; m++) {
     if (fakeGetTodayChecked[1][m] === true) {
@@ -15,13 +16,17 @@ let checkCounting = function () {
   }
   return result;
 };
-const checkCount = checkCounting(fakeGetTodayChecked);
+const checkCount = checkCounting();
 
 // Get Alarms List _CountdownTimer & AlarmList 내려주기
 const fakeGetAlarmList = {
   1: [false, '13:00:00', { 1: '비염약', 2: '0', 3: '환절기만 되면 이러네 에라이...' }],
   2: [false, '18:30:00', { 1: '밀키천식약', 2: '2', 3: '밀키약 너무 비싸다..ㅠ' }],
 };
+const fakeAlarmListArry = [
+  [false, '13:00:00', { 1: '비염약', 2: '0', 3: '환절기만 되면 이러네 에라이...' }],
+  [false, '18:30:00', { 1: '밀키천식약', 2: '2', 3: '밀키약 너무 비싸다..ㅠ' }],
+];
 
 const window = Dimensions.get('window');
 
@@ -56,7 +61,7 @@ const HomeScreen = () => {
             paddingBottom: 5,
             borderBottomStyle: 'solid',
             borderBottomWidth: 5,
-            borderBottomColor: '#649A8D',
+            borderBottomColor: '#6a9c90',
           }}
         >
           오늘도 잘 챙겨먹고 있나요?
@@ -72,22 +77,108 @@ const HomeScreen = () => {
         </Text>
         <View
           style={{
-            backgroundColor: 'skyblue',
-            width: window.width - 40,
-            height: 240,
+            borderColor: '#6a9c90',
+            borderStyle: 'solid',
+            borderWidth: 15,
+            width: 200,
+            height: 200,
+            marginLeft: (window.width - 200) / 2 - 20,
             marginTop: 10,
             marginBottom: 10,
-            borderRadius: 120,
+            borderRadius: 100,
             justifyContent: 'center',
             alignItems: 'center',
+            textAlign: 'center',
           }}
         >
           <CountdownTimer upcomingAlarm={fakeGetAlarmList[1]} />
         </View>
-        <AlarmList alarmsOnToday={fakeGetAlarmList} />
+        <View>
+          <Text style={{ fontSize: 18, fontWeight: 300, marginBottom: 10, marginTop: 10 }}>
+            오늘의 알람
+          </Text>
+          {/* <FlatList
+            horizontal={true}
+            style={styles.HomeAlarmList}
+            keyExtractor={(item) => item.toString()}
+            data={fakeAlarmListArry}
+            ListHeaderComponentStyle={true}
+            renderItem={({ item }) => <Alarm alarm={item} />}
+          ></FlatList> */}
+          <ScrollView horizontal={true} style={styles.HomeAlarmList}>
+            {fakeAlarmListArry.map((item, index) => (
+              <View
+                key={index}
+                style={[
+                  styles.HomeAlarm,
+                  index === 0 && { color: 'white', backgroundColor: '#6a9c90' },
+                ]}
+              >
+                <Text style={{ fontSize: 20, fontWeight: 500 }}>{item[2][1]}</Text>
+                <Text style={{ marginTop: 10, marginBottom: 10, fontWeight: 300 }}>
+                  {item[2][3]}
+                </Text>
+                <Text style={{ position: 'absolute', bottom: 20, fontWeight: 300 }}>
+                  {item[2][2]}일 마다
+                </Text>
+                <Text style={{ position: 'absolute', bottom: 20, right: 10, fontWeight: 300 }}>
+                  {item[1]}
+                </Text>
+              </View>
+            ))}
+          </ScrollView>
+        </View>
       </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  HomeAlarmList: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: '#D7E4E1',
+    borderTopLeftRadius: 30,
+    borderBottomLeftRadius: 30,
+    height: 175,
+    paddingLeft: 15,
+  },
+  HomeAlarm: {
+    padding: 10,
+    paddingTop: 20,
+    width: 145,
+    height: 145,
+    borderRadius: 30,
+    backgroundColor: '#e9efee',
+    margin: 15,
+    marginLeft: 0,
+    shadowColor: '#313131',
+    shadowOffset: {
+      width: 5,
+      height: 5,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 5,
+    elevation: 10,
+  },
+  HomeAlarmFirstChile: {
+    padding: 10,
+    paddingTop: 20,
+    width: 145,
+    height: 145,
+    borderRadius: 30,
+    backgroundColor: '#e9efee',
+    margin: 15,
+    marginLeft: 0,
+    shadowColor: '#313131',
+    shadowOffset: {
+      width: 5,
+      height: 5,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 5,
+    elevation: 10,
+  },
+});
 
 export default HomeScreen;
