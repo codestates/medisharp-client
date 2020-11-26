b1381f43c71347c0047b3328f4d4916dd7154a79
Grace Chung <hjngy0511@gmail.com>
2020-11-26 14:00:42 +0900

    #4/:art:/mainPage/CountdownTimer/구현 완료 done 1 - 초 단위까지 랜더링 done 2 - 알람 시간이 다 되었을 시 안내문구 랜더링 (1초) done 3 - 더이상 등록된 알람이 없을 경우 안내문구 랜더링 (다음 날 알람이 있을 때까지 계속) todo - 더이상 등록된 알람이 없을 경우 아에 타이머 기능이 정지될 수 있도록

--- src/components/CountdownTimer.js
+++ src/components/CountdownTimer.js
@@ -2,93 +2,103 @@ import React from 'react';
 import { View, Text, StyleSheet, Animated } from 'react-native';
 import moment from 'moment';
 
+function getRecentAlarm(AlarmListArry) {
+  //time이 정렬되어있는 fakeAlarmListArray가 인자로 넘어와서 AlarmListArry가 됨
+  for (let time of AlarmListArry) {
+    let now = moment();
+    let then = moment(time[1], 'hh:mm:ss');
+    let term = moment.duration(then.diff(now));
+    if (term.hours() >= 0 && term.minutes() >= 0 && term.seconds() >= 0) {
+      return time[1];
+    }
+  }
+}
+
 class CountdownTimer extends React.Component {
   constructor(props) {
     super(props);
-    const fakeAlarmListArry = this.props.upcomingAlarm;
-    // console.log('fake: ', fakeAlarmListArry); // [[false, '13:00:00', { 1: '비염약', 2: '0', 3: '환절기만 되면 이러네 에라이...' }],[false, '18:30:00', { 1: '밀키천식약', 2: '2', 3: '밀키약 너무 비싸다..ㅠ' }],];
-
-    const getRecentAlarm = () => {
-      for (let time of fakeAlarmListArry) {
-        //time이 정렬되어있는 fakeAlarmListArray.
-        let now = moment();
-        let then = moment(time[1], 'hh:mm');
-        let term = moment.duration(then.diff(now));
-        if (term.hours() >= 0 && term.minutes() >= 0) {
-          return time[1];
-        }
-      }
-    };
-    //이러고 있다가 0시간 0분 차이가 되면 '약 복용 시간입니다'를 잠깐 보여주고 다시 리로딩되면서
-    //getRecentAlarm이 다시 실행되어야함. 그래서 그 다음번 알람이 주어져야 한다.
     this.props = props;
-    this.timeTillDate = getRecentAlarm();
-    this.timeFormat = 'hh:mm';
+
+    this.timeTillDate = getRecentAlarm(this.props.upcomingAlarm);
+    this.timeFormat = 'hh:mm:ss';
     this.then = moment(this.timeTillDate, this.timeFormat);
     this.now = moment();
     this.hours = moment.duration(this.then.diff(this.now)).hours();
     this.minutes = moment.duration(this.then.diff(this.now)).minutes();
+    this.seconds = moment.duration(this.then.diff(this.now)).seconds();
     this.state = {
-      hours: this.hours,
+      hours: this.hour,
       minutes: this.minutes,
+      seconds: this.seconds,
     };
   }
 
   componentDidMount() {
     this.interval = setInterval(() => {
-      const then = moment(this.timeTillDate, this.timeFormat);
+      const timeTillDate = getRecentAlarm(this.props.upcomingAlarm);
+      console.log('timeTillDate: ', timeTillDate);
+      const then = moment(timeTillDate, this.timeFormat);
       const now = moment();
       const hours = moment.duration(then.diff(now)).hours();
       const minutes = moment.duration(then.diff(now)).minutes();
-
-      this.setState({ hours, minutes });
+      const seconds = moment.duration(then.diff(now)).seconds();
+      this.setState({ hours, minutes, seconds });
     }, 1000);
   }
+
   componentWillUnmount() {
     if (this.interval) {
       clearInterval(this.interval);
     }
   }
-  render() {
-    const { hours, minutes } = this.state;
 
-    if (minutes < 0) {
-      return (
-        <div>
-          <Text>오늘은 복용할 약이 없습니다. 내일도 오늘 같기를!</Text>
-        </div>
-      );
-    } else if (hours == 0 && minute == 0) {
+  render() {
+    const { hours, minutes, seconds } = this.state;
+
+    if (hours === 0 && minutes === 0 && seconds === 0) {
       return (
         <div>
           <Text>약 복용하실 시간입니다~</Text>
         </div>
       );
-    }
-    return (
-      <div>
-        <Text>
-          <h3>다음 알람까지</h3>
-        </Text>
-        <div className="countdown-wrapper">
-          {
-            <div className="countdown-item">
-              {hours}
-              <span> 시간</span>
-            </div>
-          }
-          {
-            <div className="countdown-item">
-              {minutes}
-              <span> 분</span>
-            </div>
-          }
+    } else if (!hours && !minutes && hours !== 0 && minutes !== 0) {
+      return (
+        <div>
+          <Text>오늘은 복용할 약이 없습니다. 내일도 오늘 같기를!</Text>
         </div>
-        <Text>
-          <h3>남았습니다.</h3>
-        </Text>
-      </div>
-    );
+      );
+    } else {
+      return (
+        <div>
+          <Text>
+            <h3>다음 알람까지</h3>
+          </Text>
+          <div className="countdown-wrapper">
+            {
+              <div className="countdown-item">
+                {hours}
+                <span> 시간</span>
+              </div>
+            }
+            {
+              <div className="countdown-item">
+                {minutes}
+                <span> 분</span>
+              </div>
+            }
+            {
+              <div className="countdown-item">
+                {seconds}
+                <span> 초</span>
+              </div>
+            }
+          </div>
+          <Text>
+            <h3>남았습니다.</h3>
+          </Text>
+        </div>
+      );
+    }
   }
 }
 export default CountdownTimer;
