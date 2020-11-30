674d2c5b5c21f724431cad9d0ffa2cdd0936f44d
Grace Chung <hjngy0511@gmail.com>
2020-11-30 17:09:08 +0900

    #4/:art:/Get Today Checked API/react hook 사용 done - 응답 결과 개수를 계산하여 알맞게 랜더링 todo - headers는 localhost에 저장된 token으로, params는 moment.js 사용해서 오늘날짜 -8, -11 한 것으로 다이나익 코들로 변경 필요

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -1,32 +1,10 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { View, Text, Dimensions, FlatList, StyleSheet, ScrollView } from 'react-native';
 import CountdownTimer from '../../components/CountdownTimer';
 // import AlarmList from '../../components/AlarmList';
 import Alarm from '../../components/Alarm';
 
-// Get Today Checked _주간 복용 현황
-const fakeGetTodayChecked = { 1: [true, true, true, true, false, true, false] };
-
-const totalCount = fakeGetTodayChecked[1].length;
-const checkCounting = function () {
-  let result = 0;
-  for (let m = 0; fakeGetTodayChecked[1][m] < totalCount; m++) {
-    if (fakeGetTodayChecked[1][m] === true) {
-      result++;
-    }
-  }
-  return result;
-};
-const checkCount = checkCounting();
-
-// Get Alarms List _CountdownTimer & AlarmList 내려주기
-// const fakeGetAlarmList = {
-//   1: [false, '13:00:00', { 1: '비염약', 2: '0', 3: '환절기만 되면 이러네 에라이...' }],
-//   2: [false, '18:30:00', { 1: '밀키천식약', 2: '2', 3: '밀키약 너무 비싸다..ㅠ' }],
-//   3: [false, '18:30:00', { 1: '밀키천식약', 2: '2', 3: '밀키약 너무 비싸다..ㅠ' }],
-// };
-//지금시간 : 12:43
 const fakeAlarmListArry = [
   [false, '12:44:00', { 1: '비염약', 2: '0', 3: '환절기만 되면 이러네 에라이...' }],
   //이게 마지막 알람이에요!
@@ -36,14 +14,9 @@ const fakeAlarmListArry = [
 
 const window = Dimensions.get('window');
 
-class HomeScreen extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      fakeAlarmListArry: [],
-    };
-
-    //const fakeGetTodayChecked = { 1: [true, true, true, true, false, true, false] };
+const HomeScreen = () => {
+  const [fakeGetTodayChecked, setfakeGetTodayChecked] = useState([]);
+  useEffect(() => {
     axios({
       method: 'get',
       url: 'http://127.0.0.1:5000/schedules-dates/check/today',
@@ -58,127 +31,139 @@ class HomeScreen extends React.Component {
     })
       .then((datas) => {
         console.log('today checked: ', datas);
+        setfakeGetTodayChecked(datas.data.results);
       })
       .catch((err) => {
         console.error(err);
       });
-  }
+  }, []);
+  console.log('fakeGetTodayChecked', fakeGetTodayChecked);
 
-  render() {
-    return (
+  const totalCount = fakeGetTodayChecked.length;
+  const checkCounting = function () {
+    let cnt = 0;
+    fakeGetTodayChecked.forEach(function (el) {
+      console.log(el, cnt);
+      cnt += el.check ? 1 : 0;
+    });
+    return cnt;
+  };
+  const checkCount = checkCounting();
+  console.log('checkCount: ', checkCount);
+
+  return (
+    <View
+      style={{
+        height: window.height,
+      }}
+    >
       <View
         style={{
-          height: window.height,
+          paddingLeft: 20,
+          height: window.height - 70,
         }}
       >
+        <Text
+          style={{
+            marginTop: 40,
+            fontSize: 24,
+            fontWeight: '300',
+          }}
+        >
+          약올림
+        </Text>
         <View
           style={{
-            paddingLeft: 20,
-            height: window.height - 70,
+            borderBottomStyle: 'solid',
+            borderBottomWidth: 5,
+            borderBottomColor: '#6a9c90',
+            alignSelf: 'flex-start',
           }}
         >
           <Text
             style={{
-              marginTop: 40,
-              fontSize: 24,
-              fontWeight: '300',
+              alignSelf: 'center',
+              marginTop: 5,
+              fontSize: 20,
+              fontWeight: 'bold',
+              paddingBottom: 5,
             }}
           >
-            약올림
+            오늘도 잘 챙겨먹고 있나요?
           </Text>
-          <View
-            style={{
-              borderBottomStyle: 'solid',
-              borderBottomWidth: 5,
-              borderBottomColor: '#6a9c90',
-              alignSelf: 'flex-start',
-            }}
-          >
-            <Text
-              style={{
-                alignSelf: 'center',
-                marginTop: 5,
-                fontSize: 20,
-                fontWeight: 'bold',
-                paddingBottom: 5,
-              }}
-            >
-              오늘도 잘 챙겨먹고 있나요?
-            </Text>
-          </View>
-          <Text
-            style={{
-              width: window.width * 0.85,
-              marginTop: 10,
-              marginBottom: 10,
-            }}
-          >
-            {'지난 7일 간 총 ' + totalCount + '회 중 ' + checkCount + '회 복용하셨군요 :)'}
+        </View>
+        <Text
+          style={{
+            width: window.width * 0.85,
+            marginTop: 10,
+            marginBottom: 10,
+          }}
+        >
+          {'지난 7일 간 총 ' + totalCount + '회 중 ' + checkCount + '회 복용하셨군요 :)'}
+        </Text>
+        <View
+          style={{
+            borderColor: '#6a9c90',
+            borderStyle: 'solid',
+            borderWidth: 15,
+            width: 200,
+            height: 200,
+            marginLeft: (window.width - 200) / 2 - 20,
+            marginTop: 10,
+            marginBottom: 10,
+            borderRadius: 100,
+            justifyContent: 'center',
+            alignItems: 'center',
+            textAlign: 'center',
+          }}
+        >
+          {/* <CountdownTimer upcomingAlarm={fakeGetAlarmList[1]} /> */}
+          <CountdownTimer upcomingAlarm={fakeAlarmListArry} />
+        </View>
+        <View>
+          <Text style={{ fontSize: 18, fontWeight: '300', marginBottom: 10, marginTop: 10 }}>
+            오늘의 알람
           </Text>
-          <View
-            style={{
-              borderColor: '#6a9c90',
-              borderStyle: 'solid',
-              borderWidth: 15,
-              width: 200,
-              height: 200,
-              marginLeft: (window.width - 200) / 2 - 20,
-              marginTop: 10,
-              marginBottom: 10,
-              borderRadius: 100,
-              justifyContent: 'center',
-              alignItems: 'center',
-              textAlign: 'center',
-            }}
-          >
-            {/* <CountdownTimer upcomingAlarm={fakeGetAlarmList[1]} /> */}
-            <CountdownTimer upcomingAlarm={fakeAlarmListArry} />
-          </View>
-          <View>
-            <Text style={{ fontSize: 18, fontWeight: '300', marginBottom: 10, marginTop: 10 }}>
-              오늘의 알람
-            </Text>
-            {/* <FlatList
-              horizontal={true}
-              style={styles.HomeAlarmList}
-              keyExtractor={(item) => item.toString()}
-              data={fakeAlarmListArry}
-              ListHeaderComponentStyle={true}
-              renderItem={({ item }) => <Alarm alarm={item} />}
-            ></FlatList> */}
+          {/* <FlatList
+            horizontal={true}
+            style={styles.HomeAlarmList}
+            keyExtractor={(item) => item.toString()}
+            data={fakeAlarmListArry}
+            ListHeaderComponentStyle={true}
+            renderItem={({ item }) => <Alarm alarm={item} />}
+          ></FlatList> */}
 
-            <ScrollView horizontal={true}>
-              <View style={styles.HomeAlarmList}>
-                {fakeAlarmListArry.map((item, index) => (
-                  <View
-                    key={index}
-                    style={[
-                      styles.HomeAlarm,
-                      index === 0 && { color: 'white', backgroundColor: '#6a9c90' },
-                    ]}
-                  >
-                    <Text style={[styles.firstItemInAlarm, index === 0 && { color: 'white' }]}>
-                      {item[2][1]}
-                    </Text>
-                    <Text style={[styles.secondItemInAlarm, index === 0 && { color: 'white' }]}>
-                      {item[2][3]}
-                    </Text>
-                    <Text style={[styles.thirdItemInAlarm, index === 0 && { color: 'white' }]}>
-                      {item[2][2]}일 마다
-                    </Text>
-                    <Text style={[styles.fourthItemInAlarm, index === 0 && { color: 'white' }]}>
-                      {item[1]}
-                    </Text>
-                  </View>
-                ))}
-              </View>
-            </ScrollView>
-          </View>
+          <ScrollView horizontal={true}>
+            <View style={styles.HomeAlarmList}>
+              {fakeAlarmListArry.map((item, index) => (
+                <View
+                  key={index}
+                  style={[
+                    styles.HomeAlarm,
+                    index === 0 && { color: 'white', backgroundColor: '#6a9c90' },
+                  ]}
+                >
+                  <Text style={[styles.firstItemInAlarm, index === 0 && { color: 'white' }]}>
+                    {item[2][1]}
+                  </Text>
+                  <Text style={[styles.secondItemInAlarm, index === 0 && { color: 'white' }]}>
+                    {item[2][3]}
+                  </Text>
+                  <Text style={[styles.thirdItemInAlarm, index === 0 && { color: 'white' }]}>
+                    {item[2][2]}일 마다
+                  </Text>
+                  <Text style={[styles.fourthItemInAlarm, index === 0 && { color: 'white' }]}>
+                    {item[1]}
+                  </Text>
+                </View>
+              ))}
+            </View>
+          </ScrollView>
         </View>
       </View>
-    );
-  }
-}
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   HomeAlarmList: {
