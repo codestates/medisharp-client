cebdfd72964e9bdfb0f2add29068d0b065c604ee
Grace Chung <hjngy0511@gmail.com>
2020-12-17 16:39:44 +0900

    #10/:art:/알람삭제/기능구현 준비

--- src/screens/AlarmUpdateScreen/index.js
+++ src/screens/AlarmUpdateScreen/index.js
@@ -58,7 +58,7 @@ export default class AlarmUpdateScreen extends React.Component {
     get_token().then((token) => {
       axios({
         method: 'get',
-        url: 'http://127.0.0.1:5000/schedules-commons',
+        url: 'https://hj-medisharp.herokuapp.com/schedules-commons',
         headers: {
           Authorization: token,
         },
@@ -80,7 +80,7 @@ export default class AlarmUpdateScreen extends React.Component {
 
           axios({
             method: 'get',
-            url: 'http://127.0.0.1:5000/medicines',
+            url: 'https://hj-medisharp.herokuapp.com/medicines',
             headers: {
               Authorization: token,
             },
--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -23,7 +23,7 @@ const HomeScreen = ({ navigation }) => {
     get_token().then((token) => {
       axios({
         method: 'get',
-        url: 'http://127.0.0.1:5000/schedules-dates/check/today',
+        url: 'https://hj-medisharp.herokuapp.com/schedules-dates/check/today',
         //https://yag-ollim.herokuapp.com/ -> 배포용 주소
         headers: {
           Authorization: token,
@@ -43,7 +43,7 @@ const HomeScreen = ({ navigation }) => {
       get_token().then((token) => {
         axios({
           method: 'get',
-          url: `http://127.0.0.1:5000/schedules-dates/schedules-commons/alarm`,
+          url: `https://hj-medisharp.herokuapp.com/schedules-dates/schedules-commons/alarm`,
           //https://yag-ollim.herokuapp.com/ -> 배포용 주소
           headers: {
             Authorization: token,
